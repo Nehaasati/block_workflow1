c1b1651ab71871b1e98219c77ecc2d775c9df36e
Nehaasati <31asatineha@gmail.com>
2025-08-01 01:39:32 +0200

    add file

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\nehaa\block_workflow1\venv
+set VIRTUAL_ENV=C:\Users\nehaa\blog_workflow1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
